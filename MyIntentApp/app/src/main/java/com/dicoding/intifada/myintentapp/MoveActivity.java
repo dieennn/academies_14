ab2618b9ebd906d89a7d140ce78d6c6876e373c5
Dien <dien_kamal@ymail.com>
2020-02-03 11:05:00 +0700

    Initial Commit

---
+++ MyIntentApp/app/src/main/java/com/dicoding/intifada/myintentapp/MoveActivity.java
@@ -0,0 +1,18 @@
+package com.dicoding.intifada.myintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MoveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+
+
+}
