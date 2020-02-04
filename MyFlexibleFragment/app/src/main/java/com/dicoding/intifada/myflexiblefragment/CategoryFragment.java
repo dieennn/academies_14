b11632946072cf12f0c843a8ed701b44a8445d35
Dien <dien_kamal@ymail.com>
2020-02-04 23:27:21 +0700

    Mengirim Data Antar Fragment

--- MyFlexibleFragment/app/src/main/java/com/dicoding/intifada/myflexiblefragment/CategoryFragment.java
+++ MyFlexibleFragment/app/src/main/java/com/dicoding/intifada/myflexiblefragment/CategoryFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -40,6 +41,28 @@ public class CategoryFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_detail_category){
+            DetailCategoryFragment mDetailCategoryFragment = new DetailCategoryFragment();
+            Bundle mBundle = new Bundle();
+            mBundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle");
+            String description = "Kategori ini akan berisi produk-produk lifestyle";
+            mDetailCategoryFragment.setArguments(mBundle);
+            mDetailCategoryFragment.setDescription(description);
+
+            /*
+            Method addToBackStack akan menambahkan fragment ke backstack
+            Behaviour dari back button akan cek fragment dari backstack,
+            jika ada fragment di dalam backstack maka fragment yang akan di close / remove
+            jika sudah tidak ada fragment di dalam backstack maka activity yang akan di close / finish
+             */
+            FragmentManager mFragmentManager = getFragmentManager();
+            if (mFragmentManager != null) {
+                mFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_container, mDetailCategoryFragment, DetailCategoryFragment.class.getSimpleName())
+                        .addToBackStack(null)
+                        .commit();
+            }
+
         }
     }
 }
