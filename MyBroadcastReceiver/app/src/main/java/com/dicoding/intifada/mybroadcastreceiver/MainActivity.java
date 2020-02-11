cb5cca58ec908d9a0ffe038c088d3a5055bf1333
Dien <dien_kamal@ymail.com>
2020-02-11 22:08:09 +0700

    Codelab Broadcast dari SMS

--- MyBroadcastReceiver/app/src/main/java/com/dicoding/intifada/mybroadcastreceiver/MainActivity.java
+++ MyBroadcastReceiver/app/src/main/java/com/dicoding/intifada/mybroadcastreceiver/MainActivity.java
@@ -3,12 +3,25 @@ package com.dicoding.intifada.mybroadcastreceiver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnCheckPermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCheckPermission = findViewById(R.id.btn_permission);
+        btnCheckPermission.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_permission) {
+        }
     }
 }
