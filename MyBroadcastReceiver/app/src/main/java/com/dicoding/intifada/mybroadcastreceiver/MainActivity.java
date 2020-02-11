1d277624bf2f54129fa317ca91727a00fd81306c
Dien <dien_kamal@ymail.com>
2020-02-11 22:21:55 +0700

    Codelab Broadcast dari Sebuah Event

--- MyBroadcastReceiver/app/src/main/java/com/dicoding/intifada/mybroadcastreceiver/MainActivity.java
+++ MyBroadcastReceiver/app/src/main/java/com/dicoding/intifada/mybroadcastreceiver/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,6 +19,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     Button btnCheckPermission;
 
+    private BroadcastReceiver downloadReceiver;
+
+    Button btnDownload;
+    public static final String ACTION_DOWNLOAD_STATUS = "download_status";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +31,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnCheckPermission = findViewById(R.id.btn_permission);
         btnCheckPermission.setOnClickListener(this);
+        btnDownload = findViewById(R.id.btn_download);
+        btnDownload.setOnClickListener(this);
+
+        downloadReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(DownloadService.TAG, "Download Selesai");
+                Toast.makeText(context, "Download Selesai", Toast.LENGTH_SHORT).show();
+            }
+        };
+        IntentFilter downloadIntentFilter = new IntentFilter(ACTION_DOWNLOAD_STATUS);
+        registerReceiver(downloadReceiver, downloadIntentFilter);
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_permission) {
             SmsReceiver.PermissionManager.check(this, Manifest.permission.RECEIVE_SMS, SMS_REQUEST_CODE);
+        } else if (v.getId() == R.id.btn_download) {
+            Intent downloadServiceIntent = new Intent(this, DownloadService.class);
+            startService(downloadServiceIntent);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (downloadReceiver != null) {
+            unregisterReceiver(downloadReceiver);
         }
     }
 
