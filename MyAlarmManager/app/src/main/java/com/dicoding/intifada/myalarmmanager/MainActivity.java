456910abbc7b0f3be561f691b6c95b0e7434e699
Dien <dien_kamal@ymail.com>
2020-02-11 23:15:00 +0700

    Codelabs Batalkan Alarm Manager

--- MyAlarmManager/app/src/main/java/com/dicoding/intifada/myalarmmanager/MainActivity.java
+++ MyAlarmManager/app/src/main/java/com/dicoding/intifada/myalarmmanager/MainActivity.java
@@ -27,6 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageButton  btnRepeatingTime;
     Button btnSetRepeating;
 
+    Button btnCancelRepeating;
+
     private AlarmReceiver alarmReceiver;
 
     @Override
@@ -52,6 +54,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnRepeatingTime.setOnClickListener(this);
         btnSetRepeating.setOnClickListener(this);
+
+        btnCancelRepeating = findViewById(R.id.btn_cancel_repeating_alarm);
+        btnCancelRepeating.setOnClickListener(this);
 
         alarmReceiver = new AlarmReceiver();
     }
@@ -90,6 +95,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 String repeatMessage = edtRepeatingMessage.getText().toString();
                 alarmReceiver.setRepeatingAlarm(this, AlarmReceiver.TYPE_REPEATING,
                         repeatTime, repeatMessage);
+                break;
+            case R.id.btn_cancel_repeating_alarm:
+                alarmReceiver.cancelAlarm(this, AlarmReceiver.TYPE_REPEATING);
                 break;
         }
     }
