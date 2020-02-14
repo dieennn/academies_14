2058cda1a86def3ed549e85d62404f5195790645
Dien <dien_kamal@ymail.com>
2020-02-14 23:14:00 +0700

    Codelab ViewModel

---
+++ MyViewModel2/app/src/main/java/com/dicoding/intifada/myviewmodel/adapter/WeatherAdapter.java
@@ -0,0 +1,69 @@
+package com.dicoding.intifada.myviewmodel.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.dicoding.intifada.myviewmodel.R;
+import com.dicoding.intifada.myviewmodel.model.WeatherItems;
+
+import java.util.ArrayList;
+
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder> {
+    private final ArrayList<WeatherItems> mData = new ArrayList<>();
+
+    public void setData(ArrayList<WeatherItems> items) {
+        mData.clear();
+        mData.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    public void addItem(final WeatherItems item) {
+        mData.add(item);
+        notifyDataSetChanged();
+    }
+
+    public void clearData() {
+        mData.clear();
+    }
+
+    @NonNull
+    @Override
+    public WeatherViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int position) {
+        View mView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.weather_items, viewGroup, false);
+        return new WeatherViewHolder(mView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherViewHolder weatherViewHolder, int position) {
+        weatherViewHolder.bind(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    class WeatherViewHolder extends RecyclerView.ViewHolder {
+        private final TextView textViewCity;
+        private final TextView textViewTemperature;
+        private final TextView textViewDescription;
+
+        WeatherViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewCity = itemView.findViewById(R.id.textCity);
+            textViewTemperature = itemView.findViewById(R.id.textTemp);
+            textViewDescription = itemView.findViewById(R.id.textDesc);
+        }
+
+        void bind(WeatherItems weatherItems){
+            textViewCity.setText(weatherItems.getName());
+            textViewTemperature.setText(weatherItems.getTemperature());
+            textViewDescription.setText(weatherItems.getDescription());
+        }
+    }
+}
