e859c65c6cfd73720904d084010e5446a6ab75b8
Dien <dien_kamal@ymail.com>
2020-02-20 22:14:47 +0700

    Codelab

---
+++ MyReadWriteFile/app/src/main/java/com/dicoding/intifada/myreadwritefile/FileModel.java
@@ -0,0 +1,20 @@
+package com.dicoding.intifada.myreadwritefile;
+
+public class FileModel {
+
+    private String filename;
+    private String data;
+    String getFilename() {
+        return filename;
+    }
+    void setFilename(String filename) {
+        this.filename = filename;
+    }
+    public String getData() {
+        return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+
+}
