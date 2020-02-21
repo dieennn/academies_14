25884463768027bcc705f84c39a2687953e282af
Dien <dien_kamal@ymail.com>
2020-02-22 02:03:32 +0700

    Content Provider : Latihan - Aplikasi Catatan Sederhana dengan Content Provider (Consumer App)

---
+++ MyNotesApp/consumerapp/src/main/java/com/dicoding/intifada/consumerapp/db/DatabaseContract.java
@@ -0,0 +1,30 @@
+package com.dicoding.intifada.consumerapp.db;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+
+    public static final String AUTHORITY = "com.dicoding.intifada.mynotesapp";
+    private static final String SCHEME = "content";
+
+    private DatabaseContract(){}
+
+    public static final class NoteColumns implements BaseColumns {
+        public static final String TABLE_NAME = "note";
+
+        //Note title
+        public static final String TITLE = "title";
+        //Note description
+        public static final String DESCRIPTION = "description";
+        //Note date
+        public static final String DATE = "date";
+
+        // untuk membuat URI content://com.dicoding.picodiploma.mynotesapp/note
+        public static final Uri CONTENT_URI = new Uri.Builder().scheme(SCHEME)
+                .authority(AUTHORITY)
+                .appendPath(TABLE_NAME)
+                .build();
+
+    }
+}
