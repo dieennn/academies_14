ba11f23613cfe97dee79c0313a46026fbf3a073e
Dien <dien_kamal@ymail.com>
2020-02-24 01:11:05 +0700

    Codelab Custom Notifikasi

--- CustomNotif/app/src/main/java/com/dicoding/intifada/customnotif/MainActivity.java
+++ CustomNotif/app/src/main/java/com/dicoding/intifada/customnotif/MainActivity.java
@@ -1,7 +1,11 @@
 package com.dicoding.intifada.customnotif;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonNotif = findViewById(R.id.button_show_notification);
+        buttonNotif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, NotificationService.class));
+            }
+        });
     }
 }
