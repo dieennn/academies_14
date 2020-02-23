d772f0d47b08c462dd8e2f5486eb28afa21b01c4
Dien <dien_kamal@ymail.com>
2020-02-24 00:17:24 +0700

    Codelab StackView Widget

--- MyStackWidget/app/src/main/java/com/dicoding/intifada/mystackwidget/MainActivity.java
+++ MyStackWidget/app/src/main/java/com/dicoding/intifada/mystackwidget/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dicoding.intifada.mystackwidget;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
